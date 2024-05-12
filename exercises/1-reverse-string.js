a4c7be75fd21979a190063f70061c912b5e13b60
uzzal71 <uzzalroy.acm@gmail.com>
2024-05-12 18:15:34 +0600

    all exercises create

---
+++ exercises/1-reverse-string.js
@@ -0,0 +1,7 @@
+// --- Directions
+// Given a string, return a new string with the reversed order of characters
+// --- Examples
+//   reverse('hi') === 'ih'
+//   reverse('hello') === 'olleh'
+
+function reverse(str) {}
