4510508a117b5e9b2b15ad32c97c57e58d8d5f43
uzzal71 <uzzalroy.acm@gmail.com>
2024-05-27 13:07:49 +0600

    binary search

---
+++ algorithms/binary-search.js
@@ -0,0 +1,21 @@
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    console.log(`start: ${start}, end: ${end}, mid: ${mid}`);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearch([-5, 2, 4, 6, 10], 10));
