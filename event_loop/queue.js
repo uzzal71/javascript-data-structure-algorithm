f2adb1ba69df91387f23b182a5ffd29764d6139d
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-28 23:28:08 +0600

    event loop

---
+++ event_loop/queue.js
@@ -0,0 +1,12 @@
+/**
+ * Event loop
+ * setImmediate
+ * process.nextTick
+ * setTimeout
+ */
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+add(10, 5);
---
+++ graph/graph-overview.js
@@ -0,0 +1,12 @@
+/**
+ * A graph is a non-linear data structure that consists of a finite number of vertices(also called nodes) connected by edges
+ * Trees are a specific type of graph data structure
+ * Graph Visualization
+ *   B
+ * /    \
+ * C    C
+ * 
+ * Type of Graph
+ * 1. directed
+ * 2. undirected
+ */
