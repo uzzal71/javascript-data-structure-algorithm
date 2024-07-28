2adccd9b6d3de531275ad10305e8465d4a8ca763
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-28 23:33:22 +0600

    event loop

--- event_loop/queue.js
+++ event_loop/queue.js
@@ -9,4 +9,16 @@ function add(a, b) {
     console.log(a + b);
 }
 
-add(10, 5);
+setImmediate(() => {
+    add(10, 5);
+});
+
+setTimeout(() => {
+    add(10, 9);
+});
+
+process.nextTick(() => {
+    add(10, 2);
+});
+
+console.log("Program has ended");
